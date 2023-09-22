652d1ac3760f7e29fa21bd1d051c070e63387f64
Tola Awofolu <oawofolu@pivotal.io>
2023-09-22 12:03:59 -0400

    API updates

--- app/api.py
+++ app/api.py
@@ -6,8 +6,10 @@ import json
 import mlflow
 import os
 import requests
+from dotenv import load_dotenv
 
 api_app = FastAPI()
+load_dotenv()
 
 # Enable CORS
 api_app.add_middleware(
@@ -20,22 +22,27 @@ api_app.add_middleware(
 
 
 @api_app.post('/inference')
-async def predict(model_name):
-    logging.info("In inference...")
+async def predict(model_name, data):
+    try:
+        logging.info("In inference...")
 
-    logging.error("Retrieving production model if exists...")
-    model_api_uri = f'{os.getenv("MLFLOW_TRACKING_URI")}/api/2.0/mlflow/registered-models/get?name={model_name}'
-    models = requests.get(model_api_uri).json()
-    prod_model_name = next((x['name'] for x in models['registered_model']['latest_versions'] if x['current_stage'].lower() == 'production'), None)
+        logging.error("Retrieving production model if exists...")
 
-    if prod_model_name:
-        logging.error(f"Found model {model_name} in Production stage...")
+        model_api_uri = f'{os.getenv("MLFLOW_TRACKING_URI")}/api/2.0/mlflow/registered-models/get?name={model_name}'
+        models = requests.get(model_api_uri).json()
+        prod_model_name = next((x['name'] for x in models['registered_model']['latest_versions'] if x['current_stage'].lower() == 'production'), None)
 
-        loaded_model = mlflow.pyfunc.load_model(model_uri=f"models:/{model_name}/Production")
-        unwrapped_model = loaded_model.unwrap_python_model()
-        result = unwrapped_model.predict(None)
-        logging.error(f"Result:\n{result}")
+        if prod_model_name:
+            logging.error(f"Found model {model_name} in Production stage...")
 
-        return result
-    else:
-        return []
+            loaded_model = mlflow.pyfunc.load_model(model_uri=f"models:/{model_name}/Production")
+            unwrapped_model = loaded_model.unwrap_python_model()
+            result = unwrapped_model.predict(data)
+            logging.error(f"Result:\n{result}")
+    
+            return result
+    except Exception as ee:
+        logging.error("An Exception occurred...", exc_info=True)
+        logging.error(str(ee))
+    return "No production-ready model was found."
+
